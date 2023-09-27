8f3f2f569f888ae5d6373f4258853201a7cd4950
ramiro cuevas <ramiroc@gmail.com>
2023-09-27 08:05:22 -0400

    [add] Vamasol login register finalizado corregido para despliegue3

--- app/dashboard-Admin/insumos/page.jsx
+++ app/dashboard-Admin/insumos/page.jsx
@@ -18,33 +18,15 @@ async function getInsumos(){
     console.log("Error cargando insumos",error)
   }
 }
-async function getProvidersName(){
-    const apiUrl = process.env.API_URL;
-    try {
-      const res = await fetch(`${apiUrl}/api/proveedores`, {
-        method: 'GET',
-        cache: 'no-store',
-      });
-      if(!res.ok){
-        throw new Error(res.status)
-      }
-      const data = await res.json();
-    
-      return data;
-    } catch (error) {
-      console.log("Error cargando usuarios",error)
-    }
-  }
 
 
 async function InsumosPage() {
   const {insumos} = await getInsumos();
-  const provider = await getProvidersName();
-  //console.log(provider)
+  //console.log(insumos)
   
   return (
     <div>
-        <InsumosView insumos={insumos} proveedor={provider}/>
+        <InsumosView insumos={insumos}/>
         <InsumosForm/>
     </div>
   )
