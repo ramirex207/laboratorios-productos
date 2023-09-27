1dde48ea1bc9735b287bfa7b2e749721db15d9fe
ramiro cuevas <ramiroc@gmail.com>
2023-09-27 08:23:23 -0400

    [add] Vamasol login register finalizado corregido para despliegue6

--- app/dashboard-Admin/proveedores/page.jsx
+++ app/dashboard-Admin/proveedores/page.jsx
@@ -1,30 +1,10 @@
-import ProvidersForm from '@components/proveedores/ProvidersForm'
-import ProvidersView from '@components/proveedores/ProvidersView';
 
-async function getUsers(){
-  const apiUrl = process.env.API_URL;
-  try {
-    const res = await fetch(`${apiUrl}/api/proveedores`, {
-      method: 'GET',
-      cache: 'no-store',
-    });
-    if(!res.ok){
-      throw new Error(res.status)
-    }
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log("Error cargando usuarios",error)
-  }
-}
-
-async function ProveedoresPage() {
-  const {proveedores} = await getUsers();
-  //console.log(proveedores)
+function ProveedoresPage() {
+  
   return (
     <div>
-        <ProvidersView proveedores={proveedores}/>
-        <ProvidersForm/>
+        providers page
+        
     </div>
   )
 }
