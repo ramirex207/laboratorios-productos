9f65bad4b7a3fc3f6df8c80103b3006a58572484
ramiro cuevas <ramiroc@gmail.com>
2023-09-27 10:49:21 -0400

    [add] probando metodo get

--- app/dashboard-Admin/proveedores/page.jsx
+++ app/dashboard-Admin/proveedores/page.jsx
@@ -1,10 +1,29 @@
+import ProvidersView from "@components/proveedores/ProvidersView";
 
-function ProveedoresPage() {
-  
+async function getUsers(){
+  const apiUrl = process.env.API_URL;
+  try {
+    const res = await fetch(`${apiUrl}/api/user`, {
+      method: 'GET',
+      cache: 'no-store',
+    });
+    if(!res.ok){
+      throw new Error(res.status)
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("Error cargando usuarios",error)
+  }
+
+}
+
+async function ProveedoresPage() {
+  const {Users} = await getUsers();
+ // console.log(Users)
   return (
     <div>
-        providers page
-        
+        <ProvidersView proveedores={Users}/>
     </div>
   )
 }
