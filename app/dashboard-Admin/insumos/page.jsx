8b08f2c65589c19e1992d477baffed4be0f99eca
ramiro cuevas <ramiroc@gmail.com>
2023-09-27 08:15:55 -0400

    [add] Vamasol login register finalizado corregido para despliegue5

--- app/dashboard-Admin/insumos/page.jsx
+++ app/dashboard-Admin/insumos/page.jsx
@@ -1,38 +1,11 @@
-import InsumosView from '@components/insumos/InsumosView';
 
-async function getInsumos() {
-  const apiUrl = process.env.API_URL;
-  try {
-    const res = await fetch(`${apiUrl}/api/insumos`, {
-      method: 'GET',
-      cache: 'no-store',
-    });
-    if (!res.ok) {
-      throw new Error(`Error de respuesta: ${res.status}`);
-    }
-    const data = await res.json();
-
-    return data;
-  } catch (error) {
-    console.error("Error cargando insumos:", error);
-    return { insumos: [] }; // Devuelve un objeto con una propiedad insumos vacía en caso de error.
-  }
-}
 
 async function InsumosPage() {
-  const { insumos } = await getInsumos();
-
-  if (!insumos) {
-    return (
-      <div>
-        <p>No se pudieron cargar los insumos.</p>
-      </div>
-    );
-  }
+  
 
   return (
     <div>
-      <InsumosView insumos={insumos} />
+      insumos Page
     </div>
   );
 }
